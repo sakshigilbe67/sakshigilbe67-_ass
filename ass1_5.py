5c2ceb87f3c410d945ee216b366eebe73987ead2
sakshigilbe67 <sakshigilbe67@gmail.com>
2025-11-20 22:00:50 +0530

    ass1

---
+++ ass1_5.py
@@ -0,0 +1,8 @@
+#Write a program to input temperature in Celsius and convert it into Fahrenheit.
+
+
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+
+print("Temperature in Fahrenheit:", fahrenheit)
+
