92c2a3e42155ca509febfe0cc99838fdd835e3c6
sakshigilbe67 <sakshigilbe67@gmail.com>
2025-11-20 21:58:19 +0530

    Update ass1_2.py

---
+++ ass1_2.py
@@ -0,0 +1,8 @@
+#Write a program to input your name, age, and city, 
+# then display them in a formatted sentence.
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+print(f" my name is {name}. I am {age} years old and I live in {city}.")
+
